5edfe0b53fe68b2d45ad494d0b2659d8c0bf8613
Loïc Diridollou <loic.diridollou@gmail.com>
2026-03-30 14:41:01 -0700

    GH1396 Add concat compatibility for subclasses of DataFrame

--- tests/test_pandas.py
+++ tests/test_pandas.py
@@ -404,6 +404,21 @@ def test_concat_args() -> None:
     )
 
 
+def test_frame_subclass_concat() -> None:
+    """Test concatenate subclass of DataFrame GH1396."""
+
+    class ChildDataFrame(pd.DataFrame):
+        @property
+        def _constructor(self) -> type[ChildDataFrame]:
+            return ChildDataFrame
+
+    cdf1 = ChildDataFrame(data={"a": [0]})
+    cdf2 = ChildDataFrame(data={"a": [1]})
+
+    cdf = pd.concat([cdf1, cdf2], ignore_index=True)
+    check(assert_type(cdf, ChildDataFrame), ChildDataFrame)
+
+
 def test_types_json_normalize() -> None:
     data1: list[dict[str, Any]] = [
         {"id": 1, "name": {"first": "Coleen", "last": "Volk"}},
@@ -2544,18 +2559,3 @@ def test_argmin_and_argmax_return() -> None:
     i1 = df.a.abs().argmax()
     check(assert_type(i, np.int64), np.int64)
     check(assert_type(i1, np.int64), np.int64)
-
-
-def test_frame_subclass_concat() -> None:
-    """Test concatenate subclass of DataFrame GH1396."""
-
-    class ChildDataFrame(pd.DataFrame):
-        @property
-        def _constructor(self) -> type[ChildDataFrame]:
-            return ChildDataFrame
-
-    cdf1 = ChildDataFrame(data={"a": [0]})
-    cdf2 = ChildDataFrame(data={"a": [1]})
-
-    cdf = pd.concat([cdf1, cdf2], ignore_index=True)
-    check(assert_type(cdf, ChildDataFrame), ChildDataFrame)
